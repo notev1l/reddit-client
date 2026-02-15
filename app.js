e35962af6ad40639a72c089184c39e0a2625d8b6
Viktor Skyba <skyba.viktor2016@gmail.com>
2026-02-15 21:05:33 +0200

    feat: add styles; add basic js functionality

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 console.log('global')
+
+const sidebar = document.querySelector('.sidebar')
+const sidebarBtnNames = document.querySelectorAll('.sidebar-btn-name')
+const switchBtn = document.querySelector('.switch-btn')
+const sidebarSwitchText = document.querySelector('.switch-btn-text')
+const toggleSidebarBtn = document.querySelector('.toggle-sidebar-btn')
+const toggleSidebarIcon = document.querySelector('.toggle-sidebar-icon')
+
+function switchTheme() {
+  if (theme) {
+    document.documentElement.style.setProperty('--primary-color', '#202B2D')
+    document.documentElement.style.setProperty('--text-color', '#c1121f')
+  } else {
+    document.documentElement.style.setProperty('--primary-color', '#EDA398')
+    document.documentElement.style.setProperty('--text-color', '#c1121f')
+  }
+  sidebarSwitchText.textContent = `Appearence: ${theme ? 'dark' : 'white'} theme`
+  theme = !theme
+}
+let theme = window.matchMedia('(prefers-color-scheme: dark)').matches
+document.onload = switchTheme()
+switchBtn.addEventListener('click', () => switchTheme())
+
+function toggleSidebar() {
+  [...sidebarBtnNames].map((e) => e.classList.toggle('showNames'))
+  sidebar.classList.toggle('toggleSidebar')
+  toggleSidebarIcon.classList.toggle('rotate180')
+}
+toggleSidebarBtn.addEventListener('click', () => toggleSidebar())
